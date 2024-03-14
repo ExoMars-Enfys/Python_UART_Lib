46e504531fe24f4df26859ce22a43ef4de468e80
gkollakides <ucasgko@ucl.ac.uk>
2024-03-14 08:40:25 +0000

    Updated Formatting of Uart Lib

--- uart_test.py
+++ uart_test.py
@@ -3,67 +3,69 @@
 #--------------Mech Board Artix 7 CMD Interpreter-------------------#
 #-------------------Giorgos Kollakides - MSSL-----------------------#
 import serial
-import time
+from time import sleep
 import sys
 import crc8
+import os
 
 #-------------------------Initialisation----------------------------#
-portnum = "COM4"                                                                #Serial Port initialisation
-cmdInput =   b'\x1f\x00\x00\x00\x00\x00\x00'                                  #CMD to Artix7
-responseCMD= b''                                                          #Initialising Response bytearray
-isBytessent = 0                                                                 #Initialising Bytes sent check counter
-exitflag = 0 
-errorFlag = 0                                                                   #Initialising exitflag
-hash
-crc8Frame = ''
-HashedInput = b''
-
-port = serial.Serial(port = portnum,                                            #Serial Port open
+portnum = "COM4"                                                                        #Serial Port initialisation
+cmdInput =   b'\x1f\x01\x00\x00\x00\x00\x00'                                            #CMD to Artix7
+i =0
+port = serial.Serial(port = portnum,                                                    #Serial Port Initialisation
                      baudrate=115200,
                      bytesize = serial.EIGHTBITS,
                      parity = serial.PARITY_ODD,
                      stopbits = serial.STOPBITS_ONE,
-                     timeout= 0.5)
-port.flushOutput()                                                              #Port Flushing to clear port
+                     timeout= 2)
+port.flushOutput()                                                                      #Port Flushing to clear port
 port.flushInput()
-
-
-def crc8Calculate(cmdInput) :
+text = "----------------------------------------------\n---ExoMars Rosalind Franklin Rover - Enfys---\n-----Mech Board Artix 7 CMD Interpreter-----\n----------Giorgos Kollakides - MSSL---------"
+def typewrite(text):
+         for character in text:
+                sys.stdout.write(character)
+                sys.stdout.flush()
+                sleep(0.03)       
+def load():
+        for i in range(25):
+                sys.stdout.write("-")
+                sys.stdout.flush()
+                sleep(0.03)
+#--------------------CRC8 Parity Generator--------------------------#
+def crc8Calculate(cmdInput) :        
         hash = crc8.crc8()
-        print(cmdInput, type(cmdInput))
+        print("\nThe Packet to be Parity checked : " ,cmdInput, type(cmdInput))
+        load()
         hash.update(cmdInput)
         crc8Frame = hash.digest()
-        crccheck = crc8Frame.hex()        
-        print(crccheck)
-        if crccheck.isalnum() == False:
-                print("Error with the CRC Calculation try a different packet")
-                print(crccheck)
-                exit()
-        else:
-                print("CRC is {} ({})".format(crc8Frame, type(crc8Frame)) )
-                return crc8Frame 
+        print("\nCRC is {} ({})".format(crc8Frame.hex(), type(crc8Frame)) )
+        load()
+        return crc8Frame 
 
 #------------------------Main Function-----------------------------#
-while errorFlag == 0:
-        if port.open :                                                          #Check that the port is active
-                print(" Port open and connected to: " + port.portstr)           #Print Connection Message
-                # if not int(crc8Calculate(cmdInput)) in range(0,255) :
-                #         errorFlag =1
-                #cnvrtInput = bytearray(cmdInput)
+while(1):
+        if port.open :                                                                  #Check that the port is active
+                os.system('cls')
+                typewrite(text)
+                print("\nArtix 7 Port open and connected to: " + port.portstr)          #Print Connection Message
+                print("\nAttempting to Write to Artix 7")
+                load()                
                 HashedInput = cmdInput + crc8Calculate(cmdInput)
-                print(HashedInput , type(HashedInput))
-                isBytessent = port.write(HashedInput)                              #Initialise Transmission and Transmission Counter Incrementation
-                print("Attempting to Write to Port")
-                print("Command type : ", type(HashedInput))                        #Check the format and type of the bytearray being sent
+                isBytessent = port.write(HashedInput)                                   #Initialise Transmission and Transmission Counter Incrementation
+                print("\nCommand type : ", type(HashedInput))                           #Check the format and type of the bytearray being sent
                 print("Sent" , isBytessent, "bytes in the form :", HashedInput.hex())
-                responseCMD = port.read(42)                                #Read the Response from the Artix7
-                print("\n", responseCMD.hex())                             #Output the Response from the Artix 7 to the user
-                exitflag = 1                                                    #Raise program end exit flag
+                load()
+                responseCMD = port.read(42)                                             #Read the Response from the Artix7
+                print("\n Response from Artix 7 :", responseCMD.hex())                  #Output the Response from the Artix 7 to the user
+                if len(responseCMD) == 0:
+                        print("NO RESPONSE FROM ARTIX 7 ***ERROR***")
+                exitflag = 1                                                            #Raise program end exit flag
 
-                if exitflag == 1:                                               #Exit Program
+                if exitflag == 1:                                                       #Exit Program
                         exit()
         else:
-                print("Port", port.portstr, "Failed to Open")                   #Port Connection error
+                typewrite(text)
+                print("\nPort", port.portstr, "Failed to Open")                         #Port Connection error
                 port.close()
                 exit()
 
